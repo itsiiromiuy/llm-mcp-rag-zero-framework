62bf0ae46270b6e0228e7eb926638da632902f41
itsiiromiuy ♥ <109933604+itsiiromiuy@users.noreply.github.com>
2025-04-25 19:14:11 -0500

    update to use genai

--- src/OpenAIChatClient.ts
+++ src/OpenAIChatClient.ts
@@ -1,26 +1,35 @@
-import { Tool } from "@modelcontextprotocol/sdk/types.js"
-import OpenAI from "openai"
+import { GoogleGenAI } from "@google/genai";
+import { Tool } from "@modelcontextprotocol/sdk/types.js";
+import "dotenv/config.js";
+import { logTitle } from "./util";
 
-export default class OpenAIChatClient{
-    private llm: OpenAI 
-    private model: string 
-    private messages: OpenAI.Chat.ChatCompletionAssistantMessageParam[] =[]
-    private tools: Tool[]
- 
+export default class GeminiChatClient {
+  private llm: GoogleGenAI;
+  private model: string;
+  private messages: { role: string; content: string }[] = [];
+  private tools: Tool[];
+  private apiKey: string = process.env.GOOGLE_API_KEY || "";
 
-
-
-    constructor(model: string, systemPrompt: string ='', tool: Tool[] = [], context: string=''){
-        this.llm = new OpenAI({
-            apiKey : process.env.OPEN_API_KEY,
-            baseURL: process.env.OPENAI_BASE_URL
-        });
-        this.model=model
-        this.messages.push({
-            role: 'system',
-            content: systemPrompt
-        })
-       
-
+  constructor(
+    model: string,
+    systemPrompt: string = "",
+    tools: Tool[],
+    context: string = ""
+  ) {
+    this.llm = new GoogleGenAI({
+      apiKey: this.apiKey
+    });
+    this.model = model;
+    this.tools = tools;
+    this.llm.models.generateContentStream({
+      model: "gemini-2.0-flash",
+      contents: "Explain how AI works"
+    });
+    if (systemPrompt) {
+      this.messages.push({ role: "system", content: systemPrompt });
     }
+    if (context) {
+      this.messages.push({ role: "user", content: context });
+    }
+  }
 }
