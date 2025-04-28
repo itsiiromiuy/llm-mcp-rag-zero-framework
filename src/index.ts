b2c062f1f0a44765411ce95d7abe4fe8461b3850
itsiiromiuy ♥ <109933604+itsiiromiuy@users.noreply.github.com>
2025-04-27 22:29:12 -0500

    adding MCP client

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-import {logTitle}   from './util';
+import MCPClient from "./MCPClient";
+import { logTitle } from "./utils";
+
+logTitle("STARTING MCP CLIENT");
+
+const mcpClient = new MCPClient("mcp-client", "node", ["build/index.js"]);
+await mcpClient.init();
+
+const tools = mcpClient.getTools();
+
+const prompt = "What is the capital of the moon?";
+const result = await mcpClient.callTool("capital", {
+  country: "moon"
+});
+
+console.log(result);
+await mcpClient.close();
